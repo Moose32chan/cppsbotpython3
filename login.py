d6dac9c05a9693eba9de3efb53f0d007162caa00
Moose32chan <36391841+Moose32chan@users.noreply.github.com>
2018-02-11 23:56:04 -0600

    final commit for a bit

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 #simple login with requests (Not tested)
 #
-#will be unfinished for a while cause i'm lazy :(
+#im aware it doesnt work, im learning python3 so as i know more this will get better
 #
 #Also this will probably be an early outline for the final one so it may seem a bit weird.
 #
@@ -11,7 +11,7 @@ import requests
 b = 1
 
 #server // may be used in future version
-s = input('Server:\n')
+s = ('Covfefe')
 
 #login ip // may be used in future version
 l = "172.104.66.25:6114"
@@ -20,22 +20,14 @@ if s == 'Covfefe':
         s == "45.79.161.185:6113"
         if b > 1:
                 print('Sorry only 1 bot is supported at the moment!')
-        exit()
+                
         if s != 'Covfefe':
                 print('Sorry, this server is not supported yet!')
-        exit()
 
         with requests.Session() as c: #to login the account
                         url = 'http://media.cpps.me/play/?lang=en' #filler url
-                        USER = 'd' #put username here
-                        PASS = 'd' #put password here
+                        USER = 'ISuckAtFind4' #put username here
+                        PASS = 'mason1234' #put password here
                         headers = {'user-agent': 'my-app/0.0.1'} #put header here
-                        login_data = dict(username=USER, password = PASS)  #this might work?
+                        login_data = dict(Penguin_Name=USER, Password = PASS)  #this might work?
                         c.post(url, data=login_data, headers=headers) #this might work?
-                        print('Logged in!')
-                        r.status_code
-                        if r.status_code == 101: #honestly not sure if this works lmao
-                                print('Wrong Password, Try Again.')
-                                end()
-                                if r.status_code == 100:
-                                        print('Wrong Username, Try Again.')
